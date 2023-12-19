a01e62ae58452e99ba85bad81caadad80edf8568
NgKaiZheng <111628514+ngkaizheng@users.noreply.github.com>
2023-12-19 18:39:17 +0800

    Update create_user_page.dart

--- lib/create_user_page.dart
+++ lib/create_user_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:logger/logger.dart';
@@ -6,7 +7,7 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:uuid/uuid.dart';
 import 'package:flutter/services.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_application_1/login_page.dart';
 import 'package:flutter_application_1/data/repositories/profile_repository.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -15,10 +16,33 @@ enum Gender { male, female }
 
 final List<String> positions = ['Sales', 'Account', 'Customer Services'];
 final List<String> banks = [
-  'Bank A',
-  'Bank B',
-  'Bank C'
-]; // Add your bank names here
+  'Affin Bank',
+  'Agrobank',
+  'Alliance Bank',
+  'AmBank',
+  'Bank Islam Malaysia',
+  'Bank Muamalat Malaysia',
+  'Bank Rakyat',
+  'Bank Simpanan Nasional (BSN)',
+  'CIMB Bank',
+  'Citibank',
+  'Deutsche Bank',
+  'Hong Leong Bank',
+  'HSBC Bank',
+  'Industrial and Commercial Bank of China (ICBC)',
+  'J.P. Morgan Chase Bank',
+  'Kuwait Finance House (KFH)',
+  'Maybank',
+  'MBSB Bank',
+  'OCBC Bank',
+  'Public Bank',
+  'RHB Bank',
+  'Standard Chartered Bank',
+  'Sumitomo Mitsui Banking Corporation (SMBC)',
+  'UOB Bank',
+  'United Overseas Bank (Malaysia)',
+  'Zurich Insurance Malaysia',
+];
 
 class CreateUserPage extends StatefulWidget {
   @override
@@ -49,17 +73,20 @@ class _CreateUserPageState extends State<CreateUserPage> {
   final _formKey = GlobalKey<FormState>();
   final logger = Logger();
   late String companyId; // User ID with the desired format
-  late int counter;
+  late int counter = 0;
+  ProfileRepository profileRepository = ProfileRepository();
 
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
+  final TextEditingController icController = TextEditingController();
   final TextEditingController phoneController = TextEditingController();
   final TextEditingController accountNumberController = TextEditingController();
   final TextEditingController basicSalaryController = TextEditingController();
   final TextEditingController epfNoController = TextEditingController();
-  final TextEditingController socsoNoController = TextEditingController();
+  // final TextEditingController socsoNoController = TextEditingController();
 
   String name = '';
+  String ic = '';
   String email = '';
   String phone = '';
   Gender? selectedGender;
@@ -68,6 +95,7 @@ class _CreateUserPageState extends State<CreateUserPage> {
   String? pickedImagePath;
   String imageUrl = '';
   DateTime? joiningDate;
+  String password = '';
 
   // Financial Information
   String accountNumber = '';
@@ -75,6 +103,8 @@ class _CreateUserPageState extends State<CreateUserPage> {
   num basicSalary = 0.0;
   String epfNo = '';
   String socsoNo = '';
+
+  User? managerUser = FirebaseAuth.instance.currentUser;
 
   Future<void> _selectDateofBirth(BuildContext context) async {
     final DateTime? pickedDate = await showDatePicker(
@@ -112,7 +142,58 @@ class _CreateUserPageState extends State<CreateUserPage> {
 
     // Use the fetched counter as the sequential part of the ID
     companyId = 'PF${counter.toString().padLeft(4, '0')}';
+
+    logger.i('companyId: $companyId');
   }
+
+  // Future<void> fetchLatestCounter() async {
+  //   try {
+  //     // Get data from users collection
+  //     final querySnapshot =
+  //         await FirebaseFirestore.instance.collection('users').get();
+
+  //     if (querySnapshot.docs.isNotEmpty) {
+  //       // Find the maximum counter value from existing user documents
+  //       final maxCounter = querySnapshot.docs
+  //           .map<int>((doc) =>
+  //               int.parse(doc.get('companyId').toString().substring(2)))
+  //           .reduce((value, element) => value > element ? value : element);
+
+  //       counter = maxCounter;
+  //     } else {
+  //       counter = 0; // Default to 0 if there are no existing user documents
+  //     }
+  //   } catch (e) {
+  //     print('Error fetching counter: $e');
+  //     counter = 0; // Default to 0 in case of an error
+  //   }
+  // }
+
+  // Future<void> fetchLatestCounter() async {
+  //   try {
+  //     // Get data from users collection
+  //     final querySnapshot =
+  //         await FirebaseFirestore.instance.collection('users').get();
+
+  //     if (querySnapshot.docs.isNotEmpty) {
+  //       // Find the maximum counter value from existing user documents
+  //       final maxCounter = querySnapshot.docs
+  //           .where((doc) =>
+  //               doc.get('companyId') != null) // Check if 'companyId' exists
+  //           .map<int>((doc) =>
+  //               int.tryParse(doc.get('companyId').toString().substring(2)) ?? 0)
+  //           .reduce((value, element) => value > element ? value : element);
+
+  //       counter = maxCounter;
+  //       logger.i('Counter : $counter');
+  //     } else {
+  //       counter = 0; // Default to 0 if there are no existing user documents
+  //     }
+  //   } catch (e) {
+  //     print('Error fetching counter: $e');
+  //     counter = 0; // Default to 0 in case of an error
+  //   }
+  // }
 
   Future<void> fetchLatestCounter() async {
     try {
@@ -120,20 +201,111 @@ class _CreateUserPageState extends State<CreateUserPage> {
       final querySnapshot =
           await FirebaseFirestore.instance.collection('users').get();
 
+      // Check if there is any document in the collection
       if (querySnapshot.docs.isNotEmpty) {
-        // Find the maximum counter value from existing user documents
-        final maxCounter = querySnapshot.docs
-            .map<int>((doc) =>
-                int.parse(doc.get('companyId').toString().substring(2)))
-            .reduce((value, element) => value > element ? value : element);
+        // Initialize the counter to a minimum value
 
-        counter = maxCounter;
+        // Iterate through all documents to find the maximum companyId
+        for (final document in querySnapshot.docs) {
+          if (document.data().containsKey('companyId')) {
+            final companyId = document['companyId'];
+
+            // Extract the last 4 digits and convert to a number
+            final lastFourDigits = int.parse(companyId.substring(2));
+
+            // Update the counter if the current companyId is greater
+            counter = lastFourDigits > counter ? lastFourDigits : counter;
+          }
+        }
+
+        // You can now use the counter variable as needed
+        logger.i('Latest counter: $counter');
       } else {
-        counter = 0; // Default to 0 if there are no existing user documents
+        // Handle the case when the collection is empty
+        logger.i('No documents found in the users collection.');
       }
-    } catch (e) {
-      print('Error fetching counter: $e');
-      counter = 0; // Default to 0 in case of an error
+    } catch (error) {
+      // Handle any errors that may occur during the process
+      print('Error fetching data: $error');
+    }
+  }
+
+  // Future signUp() async {
+  //   try {
+  //     UserCredential userCredential = await FirebaseAuth.instance
+  //         .createUserWithEmailAndPassword(
+  //             email: email.trim(), password: password.trim());
+  //     logger.i(email);
+  //     logger.i(password);
+  //     // Send email verification
+  //     await userCredential.user!.sendEmailVerification();
+  //   } on FirebaseAuthException catch (e) {
+  //     print(e);
+  //   }
+  // }
+
+  Future<void> signUp() async {
+    try {
+      // Store reference to the currently signed-in user (manager)
+      logger.i('managerUser ${managerUser}');
+
+      // // Show a loading indicator
+      // showDialog(
+      //   context: context,
+      //   builder: (context) => AlertDialog(
+      //     content: CircularProgressIndicator(),
+      //   ),
+      //   barrierDismissible: false, // Prevent user from dismissing the dialog
+      // );
+
+      // Create the new user
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+
+      // // Send email verification
+      // await FirebaseAuth.instance.currentUser!.sendEmailVerification();
+
+      // Close the loading indicator dialog
+      // Navigator.of(context).pop();
+
+      // Sign back in as the manager
+      if (managerUser != null) {
+        // Show a loading indicator
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: Text('Completing create user...'),
+          ),
+          barrierDismissible: false, // Prevent user from dismissing the dialog
+        );
+
+        // await managerUser?.reload(); // Refresh the user data
+        // logger.i('reload ${managerUser?.reload()}');
+        String _password = await ProfileRepository()
+            .getUserPasswordByEmail(managerUser!.email!);
+        logger.i('manger email ${managerUser!.email!}');
+        logger.i('manager uid $_password');
+        UserCredential userCredential =
+            await FirebaseAuth.instance.signInWithCredential(
+          EmailAuthProvider.credential(
+              email: managerUser!.email!,
+              password: _password // Use the UID as a "dummy" password
+              ),
+        );
+
+        // Sign-in successful
+        User? user = userCredential.user;
+        logger.i('Sign-in successful for user: ${user?.email}');
+        // await FirebaseAuth.instance.signOut();
+      }
+      // Close the loading indicator dialog
+      Navigator.of(context).popUntil((route) => route.isFirst);
+    } on FirebaseAuthException catch (e) {
+      logger.i(e);
+      // Close the loading indicator dialog
+      Navigator.of(context).popUntil((route) => route.isFirst);
     }
   }
 
@@ -147,8 +319,30 @@ class _CreateUserPageState extends State<CreateUserPage> {
           context: context,
           builder: (context) {
             return AlertDialog(
-              title: Text('Error'),
+              title: Text('Empty Image'),
               content: Text('Please upload an image.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+        return;
+      }
+
+      if (await ProfileRepository().checkDuplicateEmail(email)) {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Duplicate Email'),
+              content:
+                  Text('Email already exists. Please enter a different email.'),
               actions: [
                 TextButton(
                   onPressed: () {
@@ -172,8 +366,8 @@ class _CreateUserPageState extends State<CreateUserPage> {
       logger.i(birthMonth);
       String birthDay = DateFormat('dd').format(dateOfBirth!);
       logger.i(birthDay);
-      String generatedPassword = '$firstName$birthMonth$birthDay';
-      logger.d(generatedPassword);
+      password = '$firstName$birthMonth$birthDay';
+      logger.d(password);
 
       // Upload the image and get the imageUrl
       imageUrl =
@@ -196,11 +390,13 @@ class _CreateUserPageState extends State<CreateUserPage> {
       //   'password': generatedPassword, // Include the generated password
       // });
       // Create user with the collected data
+
       await ProfileRepository().createUser(
         companyId,
         {
           'name': name.formatName(),
           'email': email,
+          'ic': ic,
           'phone': phone,
           'gender': selectedGender
               ?.toString()
@@ -212,23 +408,33 @@ class _CreateUserPageState extends State<CreateUserPage> {
           'position': selectedPosition,
           'image': imageUrl,
           'companyId': companyId,
-          'password': generatedPassword, // Include the generated password
+          'password': password, // Include the generated password
+          'status': true,
+          'annualLeaveBalance': 10,
         },
         {
           'basicSalary': basicSalary,
           'epfNo': epfNo,
-          'socsoNo': socsoNo,
-          'effectiveDate': DateTime.now(),
+          'socsoNo': ic,
+          'effectiveDate': joiningDate,
         },
         {
           'accountNumber': accountNumber,
           'bankName': selectedBank,
-          'effectiveDate': DateTime.now(),
+          'effectiveDate': joiningDate,
         },
       );
 
-      // Navigate back to the profile page
-      Navigator.pop(context);
+      signUp();
+      // Show a SnackBar to indicate successful user creation
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('User created successfully!'),
+          duration: Duration(seconds: 3), // Optional: Set the duration
+        ),
+      );
+      // // Navigate back to the profile page
+      // Navigator.pop(context);
     }
   }
 
@@ -251,6 +457,7 @@ class _CreateUserPageState extends State<CreateUserPage> {
           'Create User',
           style: TextStyle(color: Colors.black), // Set title color to black
         ),
+        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -338,9 +545,6 @@ class _CreateUserPageState extends State<CreateUserPage> {
                     return 'Please enter your email';
                   }
 
-                  // Email validation using a regular expression
-                  // This example checks for a basic email format
-                  // You may want to use a more sophisticated validation logic
                   final emailRegex = RegExp(
                     r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$',
                   );
@@ -352,6 +556,26 @@ class _CreateUserPageState extends State<CreateUserPage> {
                   return null;
                 },
                 onSaved: (value) => email = value ?? '',
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'IC Number'),
+                controller: icController,
+                keyboardType: TextInputType.phone,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  LengthLimitingTextInputFormatter(12),
+                ],
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter user IC Number';
+                  }
+
+                  return null;
+                },
+                onSaved: (value) {
+                  // Remove dashes before saving
+                  ic = value?.replaceAll('-', '') ?? '';
+                },
               ),
 
               TextFormField(
@@ -523,6 +747,11 @@ class _CreateUserPageState extends State<CreateUserPage> {
                     return null;
                   },
                   onSaved: (value) => selectedBank = value ?? '',
+                  isDense: true, // Make the dropdown menu more compact
+                  itemHeight:
+                      null, // Set to null to allow the dropdown to determine the height
+                  menuMaxHeight:
+                      200, // Set the maximum height for the dropdown menu
                 ),
               ),
 
@@ -563,8 +792,17 @@ class _CreateUserPageState extends State<CreateUserPage> {
                 keyboardType: TextInputType.phone,
                 inputFormatters: [
                   FilteringTextInputFormatter.digitsOnly,
+                  LengthLimitingTextInputFormatter(14),
                 ],
                 onSaved: (value) => epfNo = value ?? '',
+                validator: (value) {
+                  if (value != null && value.isNotEmpty) {
+                    if (value.length != 14) {
+                      return 'EPF number must be 14 digits long.';
+                    }
+                  }
+                  return null; // Return null if the field is optional and empty
+                },
               ),
 
               // Note for the user
@@ -576,22 +814,30 @@ class _CreateUserPageState extends State<CreateUserPage> {
                 ),
               ),
 
-              // SOCSO No
-              TextFormField(
-                controller: socsoNoController,
-                decoration: InputDecoration(labelText: 'SOCSO No (Optional)'),
-                keyboardType: TextInputType.text,
-                onSaved: (value) => socsoNo = value ?? '',
-              ),
+              // // SOCSO No
+              // TextFormField(
+              //   controller: socsoNoController,
+              //   decoration: InputDecoration(labelText: 'SOCSO No (Optional)'),
+              //   keyboardType: TextInputType.text,
+              //   onSaved: (value) => socsoNo = value ?? '',
+              //   validator: (value) {
+              //     if (value != null && value.isNotEmpty) {
+              //       if (value.length != 12) {
+              //         return 'EPF number must be 12 digits long.';
+              //       }
+              //     }
+              //     return null; // Return null if the field is optional and empty
+              //   },
+              // ),
 
-              // Note for the user
-              Text(
-                'Note: The SOCSO No section can be left blank if user don\'t have one.',
-                style: TextStyle(
-                  fontStyle: FontStyle.italic,
-                  color: Colors.grey,
-                ),
-              ),
+              // // Note for the user
+              // Text(
+              //   'Note: The SOCSO No section can be left blank if user don\'t have one.',
+              //   style: TextStyle(
+              //     fontStyle: FontStyle.italic,
+              //     color: Colors.grey,
+              //   ),
+              // ),
 
               const SizedBox(height: 20),
               ElevatedButton(
