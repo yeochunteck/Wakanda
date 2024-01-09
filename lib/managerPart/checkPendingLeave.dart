b8bf72d888622bc38eb369be533da6819473660f
NgKaiZheng <111628514+ngkaizheng@users.noreply.github.com>
2024-01-09 10:01:17 +0800

    Update checkPendingLeave.dart

--- lib/managerPart/checkPendingLeave.dart
+++ lib/managerPart/checkPendingLeave.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/main_page.dart';
 import 'package:toggle_switch/toggle_switch.dart';
-import 'package:flutter_application_1/managerPart/checkApprovedLeave.dart';
-import 'package:flutter_application_1/managerPart/checkRejectedLeave.dart';
 import 'package:logger/logger.dart';
 import 'package:flutter_application_1/models/data_model.dart';
 import 'package:flutter_application_1/managerPart/process_PendingLeave_page.dart';
